5ffc1f2b69d52e74eb3cfb7330ee5bd748fa26fa
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-04-04 23:59:52 +0100

    setting up admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,26 @@ const express = require("express");
 const admin = express.Router();
 
 
+admin.get("/", ( req, res, next ) => {
+    if ( req.session.____AdminLogedIn____ ) {
+        next();
+    } else {
+        res.status(200).render("admin_login");
+    }
+});
+
+admin.post("/", ( req, res ) => {
+    res.end("done");
+});
+
+admin.use((req,res,next) => {
+    if ( req.session.____AdminLogedIn____ ) {
+        next();
+    } else {
+        res.locals.err = "You are not authorized to view this section";
+        res.status(200).render("index");
+    }
+
+});
+
 module.exports = admin;
