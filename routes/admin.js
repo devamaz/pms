b21c69218c564b6bb5a466d8bb7bd8564ea20597
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-04-06 03:07:24 +0100

    handle admin login cred

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,61 @@
 const express = require("express");
-
+const crypto = require("crypto");
 const admin = express.Router();
 
 
 admin.get("/", ( req, res, next ) => {
     if ( req.session.____AdminLogedIn____ ) {
-        next();
+        res.status(200).render("admin");
     } else {
-        res.status(200).render("admin_login");
+        res.status(200).render("adminlogin");
     }
 });
 
-admin.post("/", ( req, res ) => {
-    res.end("done");
+admin.post("/", async (req,res,next) => {
+
+    let { adm_username: username, adm_password: password } = req.body;
+    const admin = req.db.collection("admin");
+
+    password = crypto.createHash("md5").update(password).digest("hex");
+
+    let result;
+
+    try {
+        result = await admin.findOne({ username, password });
+    } catch(ex) {
+        result = ex;
+    } finally {
+
+        if ( Error[Symbol.hasInstance](result) ) {
+            res.render(200).render("adminlogin", { err: "Error communicating with database" });
+            return;
+        }
+
+        if ( ! result ) {
+            next();
+            return;
+        }
+
+        req.session.____AdminLogedIn____ = true;
+        res.status(200).render("admin");
+    }
+
 });
+
 
 admin.use((req,res,next) => {
     if ( req.session.____AdminLogedIn____ ) {
         next();
     } else {
-        res.locals.err = "You are not authorized to view this section";
-        res.status(200).render("index");
+        res.locals.err = "You are not authorized to view the admin section";
+        res.status(200).render("adminlogin");
     }
 
+});
+
+admin.get("/logout", ( req, res ) => {
+    delete req.session.____AdminLogedIn____;
+    res.status(200).redirect("/admin");
 });
 
 module.exports = admin;
