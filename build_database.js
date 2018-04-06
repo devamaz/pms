d11326a8afc392888aa6c63df05896192633b5b8
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-04-06 03:06:53 +0100

    add support for handling media contents of a crime reported

--- build_database.js
+++ build_database.js
@@ -14,11 +14,6 @@ db.createCollection("admin");
 db.createCollection("police.officer");
 db.createCollection("reportedcrimes");
 
-
-
-db.users.createIndex({ username: 1, bvn: 1 }, { unique: true });
-db.police.officers.createIndex( { serviceNumber: 1 } , { unique: true } );
-
 db.crimelist.insertMany([
     {
         type: "Armed Robbery",
@@ -45,3 +40,53 @@ db.crimelist.insertMany([
         commited: 0
     }
 ]);
+
+db.users.insert({
+    picture: "",
+    firstName: "",
+    lastName: "",
+    addressOne: "",
+    addressTwo: "",
+    phoneNumber: "",
+    gender: "",
+    bvn: "",
+    dob: "",
+    username: "",
+    password: "",
+    file_id: ""
+});
+
+db.reortedCrimes.insert( {
+    
+    media: {
+        image: [
+            { name: "", file_id: "" }
+        ],
+        videos: [
+            { name: "", file_id: "" }
+        ]
+    }
+});
+
+// db.crimemedia.insert({
+//     _crime_id: "id of commited crime that this media document is related to",
+//     media: {
+//         image: [
+//             { name: "", file_id: "" }
+//         ],
+//         videos: [
+//             { name: "", file_id: "" }
+//         ]
+//     }
+// });
+
+
+// create index
+
+db.fs.files.ensureIndex( { useruploaded: 1 }, { unique: true });
+db.users.createIndex({ username: 1, bvn: 1 }, { unique: true });
+db.crimemedia.createIndex( { _crime_id: 1 } , { unique: true });
+db.police.officers.createIndex( { serviceNumber: 1 } , { unique: true } );
+
+db.users.remove({ picture: ""});
+db.crimemedia.remove({ _crime_id: { $exists: true }});
