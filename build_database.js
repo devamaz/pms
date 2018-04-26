f588ea5fb3aad79e7957273caed5f78ce99cfe35
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-04-26 23:39:01 +0100

    change literals to function call

--- build_database.js
+++ build_database.js
@@ -4,7 +4,7 @@ use pms;
 var user = "pms";
 var pwd = "pms";
 
-db.createUser({ user, pwd, roles: [ "readWrite" ]});
+db.createUser({ user, pwd, roles: [ "readWrite" ] } );
 
 db.auth(user,pwd);
 
@@ -14,87 +14,111 @@ db.createCollection("admin");
 db.createCollection("police.officers");
 db.createCollection("police.stations");
 db.createCollection("reportedcrimes");
+db.createCollection("criminals");
 
 db.admin.insert( { username: "admin", password: "5f4dcc3b5aa765d61d8327deb882cf99" } );
 
 db.crimelist.insertMany([
     {
         type: "Armed Robbery",
-        commited: 0
+        commited: Number()
     },
     {
         type: "Kidnapping",
-        commited: 0
+        commited: Number()
     },
     {
         type: "Drug Trafficking",
-        commited: 0
+        commited: Number()
     },
     {
         type: "Sex Trafficking",
-        commited: 0
+        commited: Number()
     },
     {
         type: "Illegal Gun Runnings",
-        commited: 0
+        commited: Number()
     },
     {
         type: "Murder",
-        commited: 0
+        commited: Number()
     }
 ]);
 
 db.users.insert({
-    picture: "",
-    firstName: "",
-    lastName: "",
-    addressOne: "",
-    addressTwo: "",
-    phoneNumber: "",
-    gender: "",
-    bvn: "",
-    dob: "",
-    username: "",
-    password: "",
-    file_id: ""
+    picture: String(),
+    firstName: String(),
+    lastName: String(),
+    addressOne: String(),
+    addressTwo: String(),
+    phoneNumber: String(),
+    gender: String(),
+    bvn: String(),
+    dob: String(),
+    username: String(),
+    password: String(),
+    file_id: String()
 });
 
-db.reortedcrimes.insert( {
+db.reortedcrimes.insert({
     crimes : {
-	crime_type : "",
-	crime_location : "",
-	content : ""
+	crime_type : String(),
+	crime_location : String(),
+	content : String()
     },
     media: {
         image: [
-            { name: "", file_id: "" }
+            { name: String(), file_id: String() }
         ],
         videos: [
-            { name: "", file_id: "" }
+            { name: String(), file_id: String() }
         ]
     },
-    state : "",
-    date : "",
-    reportedBy : "",
-    case_number : "",
-    officers_in_charge: []
+    state : String(),
+    date : String(),
+    reportedBy : String(),
+    case_number : String(),
+    officers_in_charge: Array()
 });
 
 
-db.police.officers.insert( {
-    firstName: "",
-    lastName: "",
-    servicesNo: "",
-    dateOfBirth: "",
-    commencementDate: "",
-    picture: "",
-    file_id: "",
-    station: "",
-    assignedTo: []
+db.police.officers.insert({
+    firstName: String(),
+    lastName: String(),
+    serviceNo: String(),
+    dateOfBirth: String(),
+    commencementDate: String(),
+    picture: String(),
+    file_id: String(),
+    station: String(),
+    assignedTo: Array()
 });
 
 db.police.stations.insert({
-    station: [ "kiwi", "kaka", "titi", "pipon" ]
+    station: Array()
+});
+
+db.criminal.criminalinfo.insert({
+    firstName: String(),
+    lastName: String(),
+    file_id: String(),
+    criminal_id: Number(),
+    commited_crimes: [{
+        crime_commited: String(),
+        station_info: [{
+            station: String(),
+            officer_in_charge: String(),
+            date_imprisoned: Date(),
+            jail_number: String()
+        }],
+        convicted_times: Number()
+    }]
+});
+
+db.news.insert({
+    title: String(),
+    content: String(),
+    date: Date()
 });
 
 // create index
@@ -102,9 +126,12 @@ db.reportedcrimes.createIndex({ case_number: 1 }, { unique: true } );
 db.users.createIndex({ username: 1, bvn: 1 }, { unique: true });
 db.crimemedia.createIndex( { _crime_id: 1 } , { unique: true });
 db.police.officers.createIndex( { serviceNo: 1 }, { unique: true } );
+db.criminal.criminalinfo.createIndex( { criminal_id: 1 }, { unique: true } );
 //db.police.createIndex( { firstName: 1, lastName: 1 } );
 
 db.users.remove({ picture: ""});
 db.police.officers.remove({firstName: ""});
-db.reportedcrimes.remove( { state: ""} );
+db.reportedcrimes.remove({ state: "" });
 db.crimemedia.remove({ _crime_id: { $exists: true }});
+db.criminal.criminalinfo.remove({ criminal_id: "" });
+db.news.remove({title: ""});
